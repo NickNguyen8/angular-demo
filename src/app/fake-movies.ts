b516e34c2ea9d7396e1e7651dd38cfffa7a64f66
Nick Nguyen <nghecastis@gmail.com>
2019-04-21 21:04:09 +0700

    update-lession9

---
+++ src/app/fake-movies.ts
@@ -0,0 +1,114 @@
+import { Movie } from '../models/movie';
+
+export const fakeMovies: Movie[] = [
+    {
+        id: 1,
+        name: 'The  Ghost and the Darkness',
+        releaseYear: 1986
+    },
+    {
+        id: 2,
+        name: 'The  Blind Side',
+        releaseYear: 2009
+    },
+    {
+        id: 3,
+        name: 'Boy and Girl',
+        releaseYear: 2019
+    },
+    {
+        id: 4,
+        name: 'Sarah Corner',
+        releaseYear: 1989
+    },
+    {
+        id: 5,
+        name: 'Let me down',
+        releaseYear: 1996
+    },
+    {
+        id: 6,
+        name: 'Let it go',
+        releaseYear: 1986
+    },
+    {
+        id: 7,
+        name: 'Mad Max',
+        releaseYear: 2009
+    },
+    {
+        id: 8,
+        name: 'Lucky Luck',
+        releaseYear: 1994
+    },
+    {
+        id: 9,
+        name: 'Man of Match',
+        releaseYear: 1989
+    },
+    {
+        id: 10,
+        name: 'Come in',
+        releaseYear: 1996
+    },
+    {
+        id: 11,
+        name: 'City Hunter',
+        releaseYear: 1996
+    },
+    {
+        id: 12,
+        name: 'You will never walk alone',
+        releaseYear: 2006
+    },
+    {
+        id: 13,
+        name: 'Việt Nam quê hương tôi',
+        releaseYear: 2015
+    },
+    {
+        id: 14,
+        name: 'Fuck you',
+        releaseYear: 1989
+    },
+    {
+        id: 15,
+        name: 'Programming is shit',
+        releaseYear: 1996
+    },
+    {
+        id: 16,
+        name: 'Catalonia and Messi',
+        releaseYear: 1987
+    },
+    {
+        id: 17,
+        name: 'Two Killers',
+        releaseYear: 2009
+    },
+    {
+        id: 18,
+        name: 'Country of United',
+        releaseYear: 2019
+    },
+    {
+        id: 19,
+        name: 'Micky mouse',
+        releaseYear: 1989
+    },
+    {
+        id: 20,
+        name: 'The Transformers',
+        releaseYear: 1996
+    },
+    {
+        id: 21,
+        name: 'Good lookin',
+        releaseYear: 1986
+    },
+    {
+        id: 22,
+        name: 'Trời xanh',
+        releaseYear: 2009
+    }
+];
---
+++ src/app/movies/movies.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit } from '@angular/core';
+import { Movie } from '../../models/movie';
+// import { fakeMovies } from '../fake-movies';
+import { MovieService } from '../movie.service';
+
+@Component({
+  selector: 'app-movies',
+  templateUrl: './movies.component.html',
+  styleUrls: ['./movies.component.css']
+})
+export class MoviesComponent implements OnInit {
+  constructor(private movieService: MovieService) { }
+
+  movie: Movie = {
+    id: 1,
+    name: 'Star Wars',
+    releaseYear: 1977
+  };
+
+  // movies = fakeMovies;
+  movies: Movie[];
+
+  selectedMovie: Movie;
+
+  getMoviesFromServices(): void {
+    //  this.movies = this.movieService.getMovies();
+    this.movieService.getMovies().subscribe(updateMovies => this.movies = updateMovies);
+  }
+
+  ngOnInit() {
+    this.getMoviesFromServices();
+  }
+  onSelect(movie: Movie): void {
+    this.selectedMovie = movie;
+    console.log(`selectedMovie = ${JSON.stringify(this.selectedMovie)}`);
+    // alert(`selectedMovie = ${JSON.stringify(this.selectedMovie)}`);
+  }
+}
